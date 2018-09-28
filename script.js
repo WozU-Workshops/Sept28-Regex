785251c6bda07c787cfadfc74dca9372f22ae8bf
Cassandra Matos <cassandra@matosmendez.com>
2018-09-28 10:41:51 -0700

    upload code base

---
+++ script.js
@@ -0,0 +1,33 @@
+// REGEX RULES
+// ^ - The caret may be used to indicate the beginning of the string.
+// $ - The dollar sign is used to mark the end of a string.
+// [] - used to define a set of characters that may match
+// ? - that the preceding character is optional
+// + - one or more of the previous character
+// * - zero or more of the previous character.
+// () - to group strings together
+// . - Any character except a newline.
+// \n - A newline character.
+// \r - A carriage return character.
+// \t - A tab character.
+// \b - A word boundary (the start or end of a word)
+// \B - Anything but a word boundary
+// \d - Any digit (same as [0-9])
+// \D - Anything but a digit (same as [^0-9])
+// \s - Single whitespace (space, tab, newline, etc.)
+// \S - Single non-whitespace
+// \w - A "word character" (same as [A-Za-z0-9_])
+// \W - A "non-word character" (same as [^a-za-z0-9_])
+// g - Global
+// i - Ignore Case
+
+
+
+// REQUIREMENTS FOR VALID FORM 
+// The first letter of the first name should be capitalized
+// The first letter of the last name should be capitalized
+// The user must enter more than one character for first and last name.
+// Phone must be in the (xxx)xxx-xxxx format 
+// Age must be between 1-130
+
+
